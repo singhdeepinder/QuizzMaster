61a0fb270dca5db62c0988b169cf8b67aa7f3b7a
singhdeepinder <deepinder.singh@prudlelabs.com>
2018-02-02 06:29:02 +0530

    Commit6

--- QuantLib-master/Docs/Examples/history_iterators.cpp
+++ QuantLib-master/Docs/Examples/history_iterators.cpp
@@ -11,6 +11,7 @@ cout << "Std. deviation:  " << s.standardDeviation() << endl;
 
 // Another possibility: print out the maximum value.
 
+
 History::const_valid_iterator max = h.vbegin(), i=max, end = h.vend();
 for (i++; i!=end; i++)
     if (i->value() > max->value())
