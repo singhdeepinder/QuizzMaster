1a70f4c131d436be8c8fa639a3821df78c56e655
singhdeepinder <deepinder.singh@prudlelabs.com>
2018-02-02 06:07:24 +0530

    commit3

--- QuantLib-master/Examples/BasketLosses/BasketLosses.cpp
+++ QuantLib-master/Examples/BasketLosses/BasketLosses.cpp
@@ -7,6 +7,7 @@
  for financial quantitative analysts and developers - http://quantlib.org/
 
  QuantLib is free software: you can redistribute it and/or modify it
+  
  under the terms of the QuantLib license.  You should have received a
  copy of the license along with this program; if not, please email
  <quantlib-dev@lists.sf.net>. The license is also available online at
