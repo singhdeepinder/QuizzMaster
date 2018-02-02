7f5719680166ec32ad8194341d49ab6d39ff3d64
singhdeepinder <deepinder.singh@prudlelabs.com>
2018-02-02 19:21:52 +0530

    Commit7

--- QuantLib-master/Docs/Examples/history_iterators.cpp
+++ QuantLib-master/Docs/Examples/history_iterators.cpp
@@ -23,6 +23,7 @@ cout << "Maximum value:  " << max->value()
 
 // or the minimum, this time the STL way:
 
+
 bool lessthan(const History::Entry& i, const History::Entry& j) {
     return i.value() < j.value();
 }
